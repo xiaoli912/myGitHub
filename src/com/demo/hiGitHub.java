b5febaa3835f12b5a1c7006df762bc2f9ae3e597
李天乐 <228938700@qq.com>
2021-06-14 10:28:02 +0800

    test

--- src/com/demo/hiGitHub.java
+++ src/com/demo/hiGitHub.java
@@ -2,6 +2,6 @@ package com.demo;
 
 public class hiGitHub {
     public static void main(String[] args) {
-        System.out.println("hello,GitHub");
+        System.out.println("GitHub,NB!");
     }
 }
